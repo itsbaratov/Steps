19ed747c6748921440c43aa861d20fdef3c39077
Bek <97820974+itsbaratov@users.noreply.github.com>
2023-11-16 01:49:28 +0100

    Initial Commit

---
+++ Steps/StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsApp.swift
+//  Steps
+//
+//  Created by Nurbek Baratov on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
